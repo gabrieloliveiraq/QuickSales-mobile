25bd5004bbf7dae5751268452489a7e4a14f1efa
Gabriel de Oliveira <gabrieloliverdev@outlook.com>
2022-11-30 23:35:19 -0300

    created modal product component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import { FlatList, TouchableOpacity } from "react-native";
 import { Text } from "../Text";
 import { products } from "../../mocks/products";
 import {
-  Product,
+  ProductContainer,
   ProductImage,
   ProductDetails,
   Separator,
@@ -10,37 +11,55 @@ import {
 } from "./styles";
 import { formatCurrency } from "../../utils/formatCurrency";
 import { PlusCircle } from "../Icons/PlusCircle";
+import { ProductModal } from "../ProductModal";
+import { Product } from "../../../types/Product";
 
 export function Menu() {
-  return (
-    <FlatList
-      data={products}
-      style={{ marginTop: 32 }}
-      contentContainerStyle={{ paddingHorizontal: 24 }}
-      keyExtractor={(product) => product._id}
-      ItemSeparatorComponent={Separator}
-      renderItem={({ item: product }) => (
-        <Product>
-          <ProductImage
-            source={{
-              uri: `http://192.168.1.3:3001/uploads/${product.imagePath}`,
-            }}
-          />
-          <ProductDetails>
-            <Text weight="600">{product.name}</Text>
-            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
-              {product.description}
-            </Text>
-            <Text size={14} weight="600">
-              {formatCurrency(product.price)}
-            </Text>
-          </ProductDetails>
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<null | Product>(null);
 
-          <AddToCartButton>
-            <PlusCircle />
-          </AddToCartButton>
-        </Product>
-      )}
-    />
+  function handleOpenModal(product: Product) {
+    setIsModalVisible(true);
+    setSelectedProduct(product);
+  }
+
+  return (
+    <>
+      <ProductModal
+        visible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}
+        product={selectedProduct}
+      ></ProductModal>
+
+      <FlatList
+        data={products}
+        style={{ marginTop: 32 }}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        keyExtractor={(product) => product._id}
+        ItemSeparatorComponent={Separator}
+        renderItem={({ item: product }) => (
+          <ProductContainer onPress={() => handleOpenModal(product)}>
+            <ProductImage
+              source={{
+                uri: `http://192.168.1.3:3001/uploads/${product.imagePath}`,
+              }}
+            />
+            <ProductDetails>
+              <Text weight="600">{product.name}</Text>
+              <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+                {product.description}
+              </Text>
+              <Text size={14} weight="600">
+                {formatCurrency(product.price)}
+              </Text>
+            </ProductDetails>
+
+            <AddToCartButton>
+              <PlusCircle />
+            </AddToCartButton>
+          </ProductContainer>
+        )}
+      />
+    </>
   );
 }
---
+++ src/components/ProductModal/index.tsx
@@ -0,0 +1,35 @@
+import { Modal } from "react-native";
+import { Text } from "../Text";
+import { Product } from "../../../types/Product";
+import { Image, CloseButton } from "./styles";
+import { Close } from "../Icons/Close";
+
+interface ProductModalProps {
+  visible: boolean;
+  onClose: () => void;
+  product: null | Product;
+}
+
+export function ProductModal({ visible, onClose, product }: ProductModalProps) {
+  if (!product) {
+    return null;
+  }
+  return (
+    <Modal
+      visible={visible}
+      animationType="slide"
+      presentationStyle="pageSheet"
+      onRequestClose={onClose}
+    >
+      <Image
+        source={{
+          uri: `http://192.168.1.3:3001/uploads/${product.imagePath}`,
+        }}
+      >
+        <CloseButton onPress={onClose}>
+          <Close />
+        </CloseButton>
+      </Image>
+    </Modal>
+  );
+}
